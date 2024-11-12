5eb692e5e2737e703ac8a73ec247302b8d4a211c
Eric Pinzur <epinzur@gmail.com>
2024-11-11 18:55:16 -0600

    commonized some fixtures

--- libs/community/tests/integration_tests/graph_vectorstores/test_chroma.py
+++ libs/community/tests/integration_tests/graph_vectorstores/test_chroma.py
@@ -1,7 +1,5 @@
 """Test of Chroma DB graph vector store class `ChromaGraphVectorStore`"""
 
-import json
-import random
 from typing import Any, Generator, Iterable, List, Optional, cast
 
 import pytest
@@ -26,119 +24,8 @@ from tests.integration_tests.cache.fake_embeddings import (
 TEST_KEYSPACE = "graph_test_keyspace"
 
 
-class ParserEmbeddings(Embeddings):
-    """Parse input texts: if they are json for a List[float], fine.
-    Otherwise, return all zeros and call it a day.
-    """
-
-    def __init__(self, dimension: int) -> None:
-        self.dimension = dimension
-
-    def embed_documents(self, texts: list[str]) -> list[list[float]]:
-        return [self.embed_query(txt) for txt in texts]
-
-    def embed_query(self, text: str) -> list[float]:
-        try:
-            vals = json.loads(text)
-        except json.JSONDecodeError:
-            return [0.0] * self.dimension
-        else:
-            assert len(vals) == self.dimension
-            return vals
-
-
-@pytest.fixture
-def embedding_d2() -> Embeddings:
-    return ParserEmbeddings(dimension=2)
-
-
-class EarthEmbeddings(Embeddings):
-    def get_vector_near(self, value: float) -> List[float]:
-        base_point = [value, (1 - value**2) ** 0.5]
-        fluctuation = random.random() / 100.0
-        return [base_point[0] + fluctuation, base_point[1] - fluctuation]
-
-    def embed_documents(self, texts: list[str]) -> list[list[float]]:
-        return [self.embed_query(txt) for txt in texts]
-
-    def embed_query(self, text: str) -> list[float]:
-        words = set(text.lower().split())
-        if "earth" in words:
-            vector = self.get_vector_near(0.9)
-        elif {"planet", "world", "globe", "sphere"}.intersection(words):
-            vector = self.get_vector_near(0.8)
-        else:
-            vector = self.get_vector_near(0.1)
-        return vector
-
-
 def _result_ids(docs: Iterable[Document]) -> List[Optional[str]]:
     return [doc.id for doc in docs]
-
-
-@pytest.fixture
-def graph_vector_store_docs() -> list[Document]:
-    """
-    This is a set of Documents to pre-populate a graph vector store,
-    with entries placed in a certain way.
-
-    Space of the entries (under Euclidean similarity):
-
-                      A0    (*)
-        ....        AL   AR       <....
-        :              |              :
-        :              |  ^           :
-        v              |  .           v
-                       |   :
-       TR              |   :          BL
-    T0   --------------x--------------   B0
-       TL              |   :          BR
-                       |   :
-                       |  .
-                       | .
-                       |
-                    FL   FR
-                      F0
-
-    the query point is meant to be at (*).
-    the A are bidirectionally with B
-    the A are outgoing to T
-    the A are incoming from F
-    The links are like: L with L, 0 with 0 and R with R.
-    """
-
-    docs_a = [
-        Document(id="AL", page_content="[-1, 9]", metadata={"label": "AL"}),
-        Document(id="A0", page_content="[0, 10]", metadata={"label": "A0"}),
-        Document(id="AR", page_content="[1, 9]", metadata={"label": "AR"}),
-    ]
-    docs_b = [
-        Document(id="BL", page_content="[9, 1]", metadata={"label": "BL"}),
-        Document(id="B0", page_content="[10, 0]", metadata={"label": "B0"}),
-        Document(id="BR", page_content="[9, -1]", metadata={"label": "BR"}),
-    ]
-    docs_f = [
-        Document(id="FL", page_content="[1, -9]", metadata={"label": "FL"}),
-        Document(id="F0", page_content="[0, -10]", metadata={"label": "F0"}),
-        Document(id="FR", page_content="[-1, -9]", metadata={"label": "FR"}),
-    ]
-    docs_t = [
-        Document(id="TL", page_content="[-9, -1]", metadata={"label": "TL"}),
-        Document(id="T0", page_content="[-10, 0]", metadata={"label": "T0"}),
-        Document(id="TR", page_content="[-9, 1]", metadata={"label": "TR"}),
-    ]
-    for doc_a, suffix in zip(docs_a, ["l", "0", "r"]):
-        add_links(doc_a, Link.bidir(kind="ab_example", tag=f"tag_{suffix}"))
-        add_links(doc_a, Link.outgoing(kind="at_example", tag=f"tag_{suffix}"))
-        add_links(doc_a, Link.incoming(kind="af_example", tag=f"tag_{suffix}"))
-    for doc_b, suffix in zip(docs_b, ["l", "0", "r"]):
-        add_links(doc_b, Link.bidir(kind="ab_example", tag=f"tag_{suffix}"))
-    for doc_t, suffix in zip(docs_t, ["l", "0", "r"]):
-        add_links(doc_t, Link.incoming(kind="at_example", tag=f"tag_{suffix}"))
-    for doc_f, suffix in zip(docs_f, ["l", "0", "r"]):
-        add_links(doc_f, Link.outgoing(kind="af_example", tag=f"tag_{suffix}"))
-    return docs_a + docs_b + docs_f + docs_t
-
 
 class CassandraSession:
     table_name: str
@@ -164,8 +51,10 @@ def graph_vector_store_angular() -> Generator[ChromaGraphVectorStore, None, None
 
 
 @pytest.fixture(scope="function")
-def graph_vector_store_earth() -> Generator[ChromaGraphVectorStore, None, None]:
-    store = ChromaGraphVectorStore(embedding_function=EarthEmbeddings())
+def graph_vector_store_earth(
+    earth_embeddings: Embeddings,
+) -> Generator[ChromaGraphVectorStore, None, None]:
+    store = ChromaGraphVectorStore(embedding_function=earth_embeddings)
     yield store
     cleanup(store=store)
 
